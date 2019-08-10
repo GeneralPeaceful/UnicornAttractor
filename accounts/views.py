8c06c42e6ebdab392ce05c0e4d20897fe413935c
Ubuntu <ubuntu@ip-172-31-34-111.ec2.internal>
2019-08-10 15:32:11 +0000

    Added error and success message functionality and styling

--- accounts/views.py
+++ accounts/views.py
@@ -11,7 +11,7 @@ def logout(request):
     Logs the user out and redirects back to the home page
     """
     auth.logout(request)
-    messages.success(request, 'You have successfully logged out')
+    messages.error(request, 'You have successfully logged out.')
     return redirect(reverse('home'))
 
 
@@ -24,23 +24,17 @@ def login(request):
     if request.method == 'POST':
         user_form = UserLoginForm(request.POST)
         if user_form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
-            if username == '' or password == '':
-                messages.error(request, 'Please enter your login credentials.')
-                return redirect('login')
-            user = auth.authenticate(username=username, password=password)
+            user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
             if user:
                 auth.login(request, user)
-                messages.success(request, "You have successfully logged in.")
+                messages.error(request, "You have successfully logged in")
                 if request.GET and request.GET['next'] !='':
                     next = request.GET['next']
                     return HttpResponseRedirect(next)
                 else:
                     return redirect(reverse('home'))
             else:
-                user_form.add_error(None, "Your username or password are incorrect.")
-                return redirect('login')
+                user_form.add_error(None, "Your username or password are incorrect")
     else:
         user_form = UserLoginForm()
     args = {'user_form': user_form, 'next': request.GET.get('next', '')}
